c1833e58b2ac8bd7a2fbf9de838507e967c0034d
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-05-04 18:08:01 +0530

    Defined the pages and its routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,33 @@
 import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import Collection from './Pages/Collection'
+import About from './Pages/About'
+import Cart from './Pages/Cart'
+import Contact from './Pages/Contact'
+import Product from './Pages/Product'
+import Login from './Pages/Login'
+import PlaceOrder from './Pages/PlaceOrder'
+import Orders from './Pages/Orders'
 
 function App() {
   
   return (
     <>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/product/:productId' element={<Product/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/place-order' element={<PlaceOrder/>} />
+        <Route path='/orders' element={<Orders/>} />
+      </Routes>
+
+    </div>
       <h1 className='text-5xl'>Hello World</h1>
     </>
   )

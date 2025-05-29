ca9387f84861636d6e8c1db3e6758b446204097e
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-05-29 21:22:20 +0530

    Updated the section in navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
 
       <img src={assets.logo} className='w-36' alt="" />
 
-      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+      <ul className='hidden sm:flex gap-5 text-2xl text-gray-700'>
             <NavLink to = '/' className='flex flex-col items-center gap-1'>
                 <p>Home</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'></hr>
